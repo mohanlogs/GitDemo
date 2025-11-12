e2d1ac750ee7da62be7c28907c8a30e445beabbe
Mohan MS <mohanlogs@gmail.com>
2025-11-12 12:22:11 +0530

    First Commit

---
+++ src/gitPractice/GitProj.java
@@ -0,0 +1,13 @@
+package gitPractice;
+
+public class GitProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git_A");
+		System.out.println("Hello Git_B");
+
+	}
+
+}
