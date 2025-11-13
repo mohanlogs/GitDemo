b14a1c5680f313d86e53290fc38011d3b61c7373
Mohan MS <mohanlogs@gmail.com>
2025-11-13 09:50:15 +0530

    develop commit gitx

---
+++ src/gitPractice/GitTestFramework.java
@@ -0,0 +1,12 @@
+package gitPractice;
+
+public class GitTestFramework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Framework test by develop branch");
+	}
+
+}
